a48ef313a9d244c74a90e3d6ebbdbbd86557e10b
Artem Dadychenko <a.dadychenko@mail.freebee.by>
2018-08-23 19:16:40 +0300

    Extract <IntroducingPanel /> component

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -4,11 +4,11 @@ import cn from 'classnames';
 import { withStyles } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import styles from './styles';
+import IntroducingPanel from '../../components/introducing-panel'
 
 import {
   misc,
   freebee,
-  map,
 } from '../../themes/images';
 
 const MainPage = ({ classes }) => (
@@ -61,55 +61,7 @@ const MainPage = ({ classes }) => (
       </div>
     </div>
 
-    <div className={classes.panel}>
-      <article className={classes.article}>
-        <section className={classes.section}>
-          <h1 className={classes.textCenter}>
-            {'Привет!'}
-          </h1>
-          <p>
-            {'Мы разрабатываем сервис, который представляет собой '}
-            <Link to="/map">карту</Link>
-            {' с отметками о различных бесплатных возможностях — от общедоступных розеток, wifi точек и туалетов, до бесплатных мероприятий.'}
-          </p>
-          <blockquote className={classes.blockquote}>
-            <p className={classes.blockquoteContent}>
-              <q>Нельзя недооценивать силу халявы.</q>
-            </p>
-            <footer className={classes.blockquoteFooter}>Эрик Картман, Южный Парк S6E9</footer>
-          </blockquote>
-          <p>
-            {'В настоящее время приложение находится в стадии разработки. Следите за обновлениями!'}
-          </p>
-          <div className={classes.icons}>
-            <div className={classes.iconContainer}>
-              <a
-                className={cn(classes.icon, classes.github)}
-                href="https://github.com/FreebeeTeam"
-              >
-                {''}
-              </a>
-              <a
-                className={cn(classes.icon, classes.vk)}
-                href="https://vk.com/freebeeapp"
-              >
-                {''}
-              </a>
-              <Link
-                className={cn(classes.icon, classes.mapIcon)}
-                to="/map"
-              >
-                <img
-                  className={classes.mapIconImg}
-                  src={map}
-                  alt="Карта с бесплатными возможностями"
-                />
-              </Link>
-            </div>
-          </div>
-        </section>
-      </article>
-    </div>
+    <IntroducingPanel />
   </div>
 );
 
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -1,5 +1,3 @@
-import { vk, github } from '../../themes/images';
-
 export default () => ({
   textCenter: {
     textAlign: 'center',
@@ -7,32 +5,6 @@ export default () => ({
   textInfo: {
     color: '#d14942',
     fontStyle: 'italic',
-  },
-  blockquote: {
-    marginBottom: '1em',
-    padding: '1em',
-    paddingTop: '0.5em',
-    borderLeft: '5px solid #d14942',
-    '&:before': {
-      display: 'none',
-    },
-  },
-  blockquoteFooter: {
-    marginTop: '.5em',
-    padding: '0',
-    color: '#333333',
-    textAlign: 'left',
-    lineHeight: '1.5',
-    fontStyle: 'italic',
-    fontSize: '1em',
-    '&:before': {
-      content: '"— "',
-    },
-  },
-  blockquoteContent: {
-    fontSize: '1em',
-    lineHeight: '1.4em',
-    textIndent: '0.5em',
   },
   sky: {
     position: 'absolute',
@@ -228,75 +200,6 @@ export default () => ({
     color: 'white',
     textDecoration: 'none',
   },
-  panel: {
-    position: 'relative',
-    top: '8%',
-    left: '8%',
-    width: '40%',
-    height: 'auto',
-    MozBorderRadius: '20px',
-    WebkitBorderRadius: '20px',
-    borderRadius: '20px',
-    background: 'rgba(228, 228, 228, 0.9)',
-    zIndex: '100',
-  },
-  article: {
-    width: '100%',
-    height: '100%',
-    marginBottom: '3em',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  section: {
-    width: '90%',
-    height: '100%',
-    marginBottom: '2em',
-    marginTop: '1.5em',
-  },
-  icons: {
-    width: '100%',
-    marginTop: '2em',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  iconContainer: {
-    width: '60%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  icon: {
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: '100%',
-    width: '50px',
-    height: '50px',
-    opacity: '0.8',
-    '&:hover': {
-      opacity: 0.9,
-    },
-  },
-  github: {
-    backgroundImage: `url(${github})`,
-  },
-  vk: {
-    backgroundImage: `url(${vk})`,
-  },
-  mapIcon: {
-    outline: 'none',
-  },
-  mapIconImg: {
-    height: '50px',
-  },
-  '@media only screen and  (max-width: 425px)': {
-    iconContainer: {
-      width: '100%',
-    },
-  },
   '@media only screen and  (max-width: 768px)': {
     sky: {
       height: '200vh',
@@ -332,20 +235,6 @@ export default () => ({
     roadsign: {
       position: 'relative',
       left: '0',
-    },
-    panel: {
-      top: '0',
-      left: '5%',
-      width: '90%',
-      height: 'auto',
-      marginTop: '5%',
-      marginBottom: '5%',
-    },
-    iconContainer: {
-      width: '70%',
-    },
-    icons: {
-      marginBottom: '2em',
     },
   },
 });
